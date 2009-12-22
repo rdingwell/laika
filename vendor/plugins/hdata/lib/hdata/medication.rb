96d164cf30b194ee12f9247b0dcebcaddc48eb85
Rob Dingwell <rob@robdingwell.org>
2009-12-22 14:50:04 -0500

    more roughing the parser, hahahahahahaha

--- vendor/plugins/hdata/lib/hdata/medication.rb
+++ vendor/plugins/hdata/lib/hdata/medication.rb
@@ -46,127 +46,54 @@ module Hdata
       #     </xs:element>
       # 
       # </xs:schema>
-      # 
-      
-      # def to_hdata(xml)
-      #       
-      #        xml.instruct! :xml, :version => '1.0'
-      #         xml.patient(:xmlns => "http://projecthdata.org/hdata/schemas/2009/06/medication",
-      #                     "xmlns:core" => Hdata::Core::NAMESPACE) do
-      #                       
-      #           xml.medication do
-      #             xml.freeTextSig()
-      #             
-      #              
-      #             if start_event.present?
-      #               xml.effectiveTime( start_event.to_s(:brief))
-      #             end
-      #             if end_event.present?
-      #               xml.effectiveTime( end_event.to_s(:brief))
-      #             end
-      #               
-      #             xml.route
-      #             xml.dose
-      #             xml.site
-      #             xml.doseRestriction
-      #             xml.productForm
-      #             xml.deliveryMethod
-      #             xml.medicationInformation do 
-      #               xml.manufacturedMaterial do
-      #                 
-      #               end
-      #               
-      #             end
-      #             xml.statusOfMedication
-      #             xml.patientInstructions
-      #             xml.orderInformation
-      #             xml.informationSource
-      #             xml.narrative
-      #           end
-      #                         
-      #        end
-      #                     
-      #     end
-      #     
-      #     
-      #     def to_c32(xml)
-      #       xml.entry {
-      #         xml.substanceAdministration("classCode" => "SBADM", "moodCode" => "EVN") {
-      #           xml.templateId("root" => "2.16.840.1.113883.10.20.1.24", "assigningAuthorityName" => "CCD")
-      #           xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.8", "assigningAuthorityName" => "HITSP/C32")
-      #           xml.id
-      #           xml.consumable {        
-      #             xml.manufacturedProduct("classCode" => "MANU") {
-      #               xml.templateId("root" => "2.16.840.1.113883.10.20.1.53", "assigningAuthorityName" => "CCD") 
-      #               xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.9", "assigningAuthorityName" => "HITSP/C32") 
-      #               xml.manufacturedMaterial("classCode" => "MMAT", "determinerCode" => "KIND") {
-      # 
-      #                if(product_code && !product_code.blank? && code_system && !code_system.blank?)
-      #                 xml.code("code" => product_code, 
-      #                          "displayName" => product_coded_display_name, 
-      #                          "codeSystem" => code_system.code, 
-      #                          "codeSystemName" => code_system.name){
-      #                              xml.originalText(product_coded_display_name)
-      #                          } 
-      #                  end         
-      #                 if free_text_brand_name 
-      #                   xml.name free_text_brand_name
-      #                 end
-      #               }
-      #             }
-      #           }
-      # 
-      #           if medication_type
-      #             xml.entryRelationship("typeCode" => "SUBJ") {
-      #               xml.observation("classCode" => "OBS", "moodCode" => "EVN") {                           
-      #                 xml.templateId("root" => "2.16.840.1.113883.3.88.11.32.10") 
-      #                 xml.code("code" => medication_type.code, 
-      #                          "displayName" => medication_type.name, 
-      #                          "codeSystem" => "2.16.840.1.113883.6.96", 
-      #                          "codeSystemName" => "SNOMED CT")
-      #                 xml.statusCode("code" => "completed")
-      #               }  
-      #             }
-      #           end
-      # 
-      #           if status
-      #             xml.entryRelationship("typeCode" => "REFR") {
-      #               xml.observation("classCode" => "OBS", "moodCode" => "EVN") {
-      #                 xml.code("code" => "33999-4", 
-      #                          "displayName" => "Status", 
-      #                          "codeSystem" => "2.16.840.1.113883.6.1", 
-      #                          "codeSystemName" => "LOINC")
-      #                 xml.statusCode("code" =>status)
-      #                 xml.value("xsi:type" => "CE", 
-      #                           "code" => "55561003", 
-      #                           "displayName" => "Active", 
-      #                           "codeSystem" => "2.16.840.1.113883.6.96", 
-      #                           "codeSystemName" => "SNOMED CT")
-      #               }
-      #             }
-      #           end
-      # 
-      #           if quantity_ordered_value  || quantity_ordered_unit  || expiration_time 
-      #             xml.entryRelationship("typeCode" => "REFR") {
-      #               xml.supply("classCode" => "SPLY", "moodCode" => "INT") {
-      #                 xml.templateId("root" => "2.16.840.1.113883.3.88.1.11.32.11")
-      #                 if quantity_ordered_unit 
-      #                   xml.id("root" => quantity_ordered_unit, "extension" => "SCRIP#")
-      #                 end 
-      #                 if expiration_time 
-      #                     xml.effectiveTime("value" => expiration_time.to_s(:brief))
-      #                 end
-      #                 if quantity_ordered_value 
-      #                   xml.quantity("value" => quantity_ordered_value)
-      #                 end  
-      #               }
-      #             }
-      #           end
-      # 
-      #         }
-      #       }
-      #     end
-      
+      #
+
+      def to_hdata(xml)
+
+        xml.instruct! :xml, :version => '1.0'
+        xml.medication(:xmlns => "http://projecthdata.org/hdata/schemas/2009/06/medication",
+        "xmlns:core" => Hdata::Core::NAMESPACE) do
+
+          if start_event.present?
+            xml.effectiveTime( start_event.to_s(:brief))
+          end
+          if end_event.present?
+            xml.effectiveTime( end_event.to_s(:brief))
+          end
+
+          xml.medicationInformation do 
+            xml.manufacturedMaterial do
+              xml.codedProductName( "code" => product_code, 
+                                    "displayName" => product_coded_display_name, 
+                                    "codeSystem" => code_system.code, 
+                                    "codeSystemName" => code_system.name) do 
+                xml.codedBrandName(:code=>"", :codeSystem=>"",:displayName => "")
+                xml.freeTextProductName free_text_brand_name if free_text_brand_name
+              end  
+              xml.freeTextBrandName free_text_brand_name if free_text_brand_name
+            end                
+          end
+
+          if medication_type.present
+
+            xml.medicationType("code" => medication_type.code, 
+                                "displayName" => medication_type.name, 
+                                "codeSystem" => "2.16.840.1.113883.6.96", 
+                               "codeSystemName" => "SNOMED CT")
+          end
+          if status
+            xml.statusOfMedication(:code=>"", :codeSystem=>"",:displayName => "")
+          end
+          xml.orderInformation do
+            if quantity_ordered_value  || quantity_ordered_unit 
+              xml.quantityOrdered(:unit=>quantity_ordered_unit, :value=>quantity_ordered_value)
+            end
+            xml.orderExperationTime expiration_time if expiration_time
+          end
+        end
+  
+      end
+    
     end
   end
 end
